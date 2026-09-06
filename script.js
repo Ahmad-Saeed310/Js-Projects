5aaa35e47632bc65214438d271e8f2f85a235839
Ahmad-Saeed310 <f23bdoc1e02215@iub.edu.pk>
2026-09-06 08:36:02 +0500

    serial,checkbox,delete

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const ordered = document.querySelector(".delete");
 const texts = document.getElementById("texts");
 const reveal = document.querySelector(".reveal");
 
-
+localStorage.setItem("1",texts.value);
 
 const task = [];
 reveal.addEventListener("click", function () {
@@ -30,33 +30,70 @@ add.addEventListener("click", function () {
   //   ((addingTODO.style.widows = "100%"), (addingTODO.style.display = "absolute"));
   //   ordered.appendChild(addingTODO);
 });
-del.addEventListener("click", function () {
-  // textField.style.display = "none";
-});
+// del.addEventListener("click", function () {
+//   // textField.style.display = "none";
+// });
 
 
 texts.addEventListener("keydown", function (event) {
-  event.preventDefault;
-  if (event.key == "Enter")
+  // event.preventDefault();
+  if (event.key === "Enter")
     {
     textField.classList.add("hidden");
     const cross = document.createElement("i");
     cross.classList.add("ri-delete-back-2-line");
+    const edit = document.createElement("i");
+    edit.classList.add("ri-pencil-line");
     // cross.innerHTML= "<i class="ri-delete-back-2-line"></i>";
     const li = document.createElement("li");
-    li.style.backgroundColor = "red";
+    li.style.backgroundColor = "";
     li.style.height = "30px";
     li.style.width = "100%";
-    li.innerHTML = texts.value;
+    li.style.display = "flex";
+    li.style.justifyItems = "center"
+    li.style.justifyContent = "space-between"
+
+    const checkes = document.createElement("input");
+    checkes.setAttribute("type","checkbox")
+    const indexed= task.length+1;
+    const serial = document.createElement("i");
+    serial.textContent = indexed;
+localStorage.setItem("1",texts.value);
+    li.textContent = localStorage.getItem("1");
     texts.value = "";
     ordered.appendChild(li);
-    ordered.appendChild(cross);
+    li.appendChild(cross);
+    li.appendChild(checkes);
+    li.appendChild(serial);
+    li.appendChild(edit);
     cross.addEventListener("click", function () {
-      task.pop();
+      // task.pop();
+      li.remove()
+      // cross.remove()
       console.log(task.length)
+      // localStorage.setItem(li);
+
     });
+    edit.addEventListener("click",function(){
+      console.log(li.textContent)
+    })
+    checkes.addEventListener("click",function(){
+      
+      if(checkes.checked){
+  
+        li.style.textDecoration =  "line-through"
+        console.log(checkes.checked)
+    }else{
+       li.style.textDecoration =  ""
+        console.log(checkes.checked)
+
+    }
+    })
     task.push(li)
     console.log(task.length+"task array")
+
+    const local = localStorage.getItem("1");
+    console.log(local)
 
   }
 });
